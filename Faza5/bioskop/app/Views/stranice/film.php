9f458b34345c0ee89d79f913747ccdff735326ef
Natalija Tosic <tn190346d@student.etf.bg.ac.rs>
2022-06-02 16:49:43 +0200

    ispravljeno prikazivanje postera kada ga nema u bazi

--- Faza5/bioskop/app/Views/stranice/film.php
+++ Faza5/bioskop/app/Views/stranice/film.php
@@ -1,12 +1,23 @@
 <!--Natalija Tosic 19/0346-->
 <?php
+if(empty($film)){}
+else{
 echo '<tr>';
+echo '<td class="poster">';
+if(!empty($film->Poster)){
 $image = imagecreatefromstring($film->Poster); 
  ob_start();
  imagejpeg($image, null, 80);
  $data = ob_get_contents();
  ob_end_clean();
- echo'       <td class="poster"><img src="data:image/png;base64,' .  base64_encode($data)  . '" height = 170 width = 120></td>';
+ echo'<img src="data:image/png;base64,' .  base64_encode($data)  . '" height = 170 width = 120>';
+} else{
+echo "<img src=";
+ echo base_url('Slike/NoImage.png');
+ echo " width='170' height='120'>";
+}
+ echo '</td>';
 echo"<td>$film->Naziv</td>
 <td><input type='radio' id='izabran' name='izabran' value ='$film->IdF' required onchange='izracunajtermine($film->IdF);'></td>
 </tr>";
+}
