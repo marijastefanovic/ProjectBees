26ef40e0b843d62f2c3f66076e22954129a31197
marijastefanovic <sm190068d@student.etf.bg.ac.rs>
2022-06-02 10:42:56 +0200

    izmena pocetne rute

--- Faza5/bioskop/app/Config/Routes.php
+++ Faza5/bioskop/app/Config/Routes.php
@@ -18,7 +18,7 @@ if (file_exists(SYSTEMPATH . 'Config/Routes.php')) {
  */
 $routes->setDefaultNamespace('App\Controllers');
 $routes->setDefaultController('Neregistrovani');
-$routes->setDefaultMethod('index');
+$routes->setDefaultMethod('pocetna');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
 $routes->setAutoRoute(true);
@@ -31,7 +31,7 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Home::pocetna');
 $routes->match(['get', 'post'], 'Slike/(:segment)', 'Image::index/$1');
 
 /*
